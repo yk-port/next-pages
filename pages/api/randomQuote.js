43365dd5a75d4fdc740cd1ea8a5514de8c82ebad
tatsuya <yokokura.tatsuya@gmail.com>
2020-04-15 21:01:52 +0900

    quotes.jsonからデータを取得してランダムで返す

--- pages/api/randomQuote.js
+++ pages/api/randomQuote.js
@@ -1,6 +1,6 @@
+import quotes from '../../quotes.json'
+
 export default (req, res) => {
-  res.status(200).json({
-    quote: 'Write tests, not too many, mostly integration',
-    author: 'Guillermo Rauch'
-  })
+  const quote = quotes[Math.floor(Math.random() * quotes.length)]
+  res.status(200).json(quote)
 }
