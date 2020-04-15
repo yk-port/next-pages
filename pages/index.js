4de564688d660b48b8eb1c21c92d12f275aca65a
tatsuya <yokokura.tatsuya@gmail.com>
2020-04-15 16:59:51 +0900

    APIのデータを取得する
    Indexに対してgetInitialPropsを使いデータを取得、mapを使ってjson形式に成形する

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
+import Layout from '../components/MyLayout'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const PostLink = props => (
-  <li>
-    <Link href="./blog/[id]" as={`/blog/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
-);
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
-      </ul>
-    </Layout>
-  );
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/tv/[id]" as={`/tv/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
 }
+
+export default Index
